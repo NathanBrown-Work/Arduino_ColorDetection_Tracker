2519aa5c3f60dc235c1d217e8cf29c2d0f129603
NathanBrown-Work <nabrownwork@gmail.com>
2026-04-10 04:16:37 -0400

    Update main.py
    Converted main into exclusively a control panel for the Arduino and initializing framework components. Also updated naming convention consistency. Finally, included a basic user input for proper pins.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk
 from pyfirmata2 import Arduino, util
 import time
 import basic_functions as bf
 import webcam as wc
-# ----------------------------------------------------
-# Initialize Python Recognition of Arduino and Tkinter
-# ----------------------------------------------------
+# -----------------------------------------------------------
+# Initialize Python Recognition of Arduino and Tkinter Window
+# -----------------------------------------------------------
 board = Arduino("COM3")
 time.sleep(2)  # Delay for initialize time
 it = util.Iterator(board)  # Background thread dedicated to reading Arduino Data
 it.start()
 win = tk.Tk()  # Initialize Window Identifier
-# ==========================================
+# ===========================================
 # PyFirmata(2) Pin .mode Documentation
 # ------------------------------------
 # 0 = INPUT ; Used for .read() ; Returns True (HIGH), False (LOW), None (Not Ready)
@@ -22,44 +22,86 @@ win = tk.Tk()  # Initialize Window Identifier
 # =========================================================================
 
 
-def ledSwitchGUI(digitalPin):  # Creates a Tkinter GUI for turning a wired LED on and off
-    """
-    ledSwitchGUI creates a GUI for turning an LED on or off via input digitalPin turning on or off
-    :param digitalPin:
-    :return:
-    """
-    # Initialize window with title and min-size
-    win.title("L E D")
+def arduinoControlPanel(digital_pin=None, servo_pin=None, wc_plugged="no"):
+    # ============ Startup ============
+    win.title("Arduino Control Panel")
     win.minsize(200, 60)
-    # Label widget and placement
-    label = tk.Label(win, text="click to turn ON/OFF")
-    label.grid(column=1, row=1)
-    # On-Off Button widgets and placements
-    ONbtn = tk.Button(win, bd=4, text="LED ON", command=lambda: bf.digPin_ON(digitalPin, board))
-    ONbtn.grid(column=1, row=2)
-    OFFbtn = tk.Button(win, bd=4, text="LED OFF", command=lambda: bf.digPin_OFF(digitalPin, board))
-    OFFbtn.grid(column=2, row=2)
-    # Run window
-    win.mainloop()
+    home_frame = tk.Frame(win)
+    home_frame.grid(row=0, column=0, sticky="nsew")
+    option_frame = tk.Frame(win)
+    option_frame.grid(row=0, column=0, sticky="nsew")
+    # ================================ Options Page ================================
+    tk.Label(option_frame, text="Mark Active Programs\n--------------------").pack()
+    tk.Button(option_frame, bd=4, text="Return Home", command=home_frame.tkraise).pack()
+    led_sw_bool = tk.BooleanVar()
+    led_sw_check = tk.Checkbutton(option_frame, text="Enable LED Switch GUI",
+                                  variable=led_sw_bool, state="disabled")
+    if digital_pin is not None:
+        led_sw_check.config(state="normal")
+    led_sw_check.pack()
+    servo_scl_bool = tk.BooleanVar()
+    servo_scl_check = tk.Checkbutton(option_frame, text="Enable Servo Motor GUI",
+                                     variable=servo_scl_bool, state="disabled")
+    if servo_pin is not None:
+        servo_scl_check.config(state="normal")
+    servo_scl_check.pack()
+    blue_track_bool = tk.BooleanVar()
+    blue_track_check = tk.Checkbutton(option_frame, text="Enable Webcam Blue Tracker",
+                                      variable=blue_track_bool, state="disabled")
+    if wc_plugged == "yes":
+        blue_track_check.config(state="normal")
+    blue_track_check.pack()
+    # ================================== Home Page ==================================
+    tk.Label(home_frame, text="This is the Home Page\n---------------------").pack()
+    tk.Button(home_frame, bd=4, text="Settings", command=option_frame.tkraise).pack()
+    ttk.Separator(home_frame, orient="horizontal").pack(fill="x", pady=10)
 
+    # -------- ledSwitchGUI --------
+    led_frame = tk.Frame(home_frame)
+    tk.Button(led_frame, bd=4, text="LED ON",
+              command=lambda: bf.digPinON(digital_pin, board)).pack(side="left")
+    tk.Button(led_frame, bd=4, text="LED OFF",
+              command=lambda: bf.digPinOFF(digital_pin, board)).pack(side="right")
 
-def servoMotorGUI(servoPin):
-    """
-    servoMotorGUI creates a GUI for rotating a Servo-Motor via 0-180 sliding bar. Input servoPin is the Servo-Motor
-    signal pin
-    :param servoPin:
-    :return:
-    """
-    # Initialize window with title and min-size
-    win.title("ServoMotor Control")
-    win.minsize(235, 150)
-    # Scale widget and placement
-    s_motor_angle = tk.Scale(win, bd=5, from_=0, to=180, orient=tk.HORIZONTAL,
-                             command=lambda val: bf.servoAngle(int(val), servoPin, board))
-    s_motor_angle.grid(column=1, row=1)
-    # Label widget and placement
-    tk.Label(win, text="Angle of ServoMotor").grid(column=1, row=2)
-    # Run window
+    def ledSwitchGUI(*args):
+        if led_sw_bool.get():
+            led_frame.pack(fill="x", pady=2)
+        else:
+            led_frame.pack_forget()
+
+    led_sw_bool.trace_add("write", ledSwitchGUI)
+    ledSwitchGUI()
+
+    # --------- servoMotorGUI ---------
+    servo_frame = tk.Frame(home_frame)
+    tk.Scale(servo_frame, bd=5, from_=0, to=180, orient=tk.HORIZONTAL,
+             command=lambda val: bf.servoAngle(int(val), servo_pin, board)).pack()
+
+    def servoMotorGUI(*args):
+        if servo_scl_bool.get():
+            servo_frame.pack(fill="x", pady=2)
+        else:
+            servo_frame.pack_forget()
+
+    servo_scl_bool.trace_add("write", servoMotorGUI)
+    servoMotorGUI()
+
+    # ------- webcamBlueTracker -------
+    webcam_frame = tk.Frame(home_frame)
+    tk.Label(webcam_frame,
+             text="Turn off the Webcam by pressing 'S'").pack()
+    tk.Button(webcam_frame, bd=4, text="Start Webcam",
+              command=lambda: wc.coloredObjTracking()).pack()
+
+    def webcamBlueTracker(*args):
+        if blue_track_bool.get():
+            webcam_frame.pack(fill="x", pady=2)
+        else:
+            webcam_frame.pack_forget()
+
+    blue_track_bool.trace_add("write", webcamBlueTracker)
+    webcamBlueTracker()
+    # ============
     win.mainloop()
 
 
@@ -67,11 +109,39 @@ if __name__ == "__main__":
     # ------------- Confirm closing ------------------------------------------
     win.protocol("WM_DELETE_WINDOW", lambda: bf.on_closing(board, win))
     # ========================================================================
-
     # tk._test()
     # bf.sanityCheck(board)
     # ledSwitchGUI(3)
     # servoMotorGUI(9)
-    # wc.color_tracking_old()
+    # wc.colorTrackingOld()
     # wc.coloredObjTracking()
+    # webcamBlueTracker()
+
+    digital_pin = (input("If you have a pin wired for an LED, enter its number here. If not, enter no: ")
+                   .strip().lower())
+    servo_pin = (input("If you have a pin wired for a Servo Motor, enter its number here. If not, enter no: ")
+                 .strip().lower())
+    wc_plugged = input("If you have a plugged in webcam, enter yes. If not, enter no: ").strip().lower()
+
+    if digital_pin == "no":
+        digital_pin = None
+    else:
+        try:
+            digital_pin = int(digital_pin)
+        except ValueError:
+            print("Invalid number for led pin")
+            exit()
+    if servo_pin == "no":
+        servo_pin = None
+    else:
+        try:
+            servo_pin = int(servo_pin)
+        except ValueError:
+            print("Invalid number for motor pin")
+            exit()
+    if wc_plugged not in ("yes", "no"):
+        print("Invalid yes/no for webcam state")
+        exit()
+
+    arduinoControlPanel(digital_pin, servo_pin, wc_plugged)
     exit()
