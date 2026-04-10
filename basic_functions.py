4d491d297260ad61d2eaee6805c1cb0a4ecb6675
NathanBrown-Work <nabrownwork@gmail.com>
2026-04-10 04:20:05 -0400

    Update basic_functions.py
    Updated naming conventions for consistency. Tacked on a placeholder function for testing purposes.

--- basic_functions.py
+++ basic_functions.py
@@ -9,10 +9,11 @@ import time
 # 4 = SERVO ; Used for .write(0-180)
 # =========================================================================
 
+
 def sanityCheck(board):  # NOTE! Upload StandardFirmware, if any code is not working, from ArduinoIDE
     """
     sanityCheck tests Python's interaction with the Arduino Mega 2560 by turning on and off the
-    on-board LED (also known as digitalPin 13). If the LED does not turn on:
+    on-board LED (also known as digital[13]). If the LED does not turn on:
     1. Double-check StandardFirmware is uploaded to your Arduino via ArduinoIDE;
     2. Double-check the COM port you are uploading to is correct;
     3. Double-check no other application using your Arduino's COM port is active (For example, close ArduinoIDE)
@@ -27,41 +28,41 @@ def sanityCheck(board):  # NOTE! Upload StandardFirmware, if any code is not wor
     print("sanityCheck has ended!")
 
 
-def digPin_ON(digitalPin, board):
+def digPinON(digital_pin, board):
     """
     digPin_ON turns on the designated digitalPin
-    :param digitalPin:
+    :param digital_pin:
     :param board:
     :return:
     """
-    board.digital[digitalPin].mode = 1
-    board.digital[digitalPin].write(1)
-    print(f"{digitalPin} On")
+    board.digital[digital_pin].mode = 1
+    board.digital[digital_pin].write(1)
+    print(f"{digital_pin} On")
 
 
-def digPin_OFF(digitalPin, board):
+def digPinOFF(digital_pin, board):
     """
     digPin_OFF turns off the designated digitalPin
-    :param digitalPin:
+    :param digital_pin:
     :param board:
     :return:
     """
-    board.digital[digitalPin].mode = 1
-    board.digital[digitalPin].write(0)
-    print(f"{digitalPin} Off")
+    board.digital[digital_pin].mode = 1
+    board.digital[digital_pin].write(0)
+    print(f"{digital_pin} Off")
 
 
-def servoAngle(angle, servoPin, board):
+def servoAngle(angle, servo_pin, board):
     """
     servoAngle writes the desired angle to the designated servoPin; Will clamp 0-180 automatically
     :param angle:
-    :param servoPin:
+    :param servo_pin:
     :param board:
     :return:
     """
-    board.digital[servoPin].mode = 4
+    board.digital[servo_pin].mode = 4
     angle = max(0, min(180, int(angle)))
-    board.digital[servoPin].write(angle)
+    board.digital[servo_pin].write(angle)
     print(f"{angle} Achieved")
 
 
@@ -74,3 +75,7 @@ def on_closing(board, win):
     """
     board.exit()
     win.destroy()
+
+
+def placeHolder():
+    print("This is placeholder text")
