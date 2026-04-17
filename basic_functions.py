2e927bb517141a407d5de8e0c5010cf54488bbd1
NathanBrown-Work <nabrownwork@gmail.com>
2026-04-17 11:34:47 -0400

    Update basic_functions.py
    Created user input function, introduction(), and created a test function for a stepper motor

--- basic_functions.py
+++ basic_functions.py
@@ -28,6 +28,39 @@ def sanityCheck(board):  # NOTE! Upload StandardFirmware, if any code is not wor
     print("sanityCheck has ended!")
 
 
+def introduction():
+    """
+    introduction asks the user to provide required information regarding the control panel's functionality
+    :return:
+    """
+    digital_pin = (input("If you have a pin wired for an LED, enter its number here. If not, enter no: ")
+                   .strip().lower())
+    servo_pin = (input("If you have a pin wired for a Servo Motor, enter its number here. If not, enter no: ")
+                 .strip().lower())
+    wc_plugged = input("If you have a plugged in webcam, enter yes. If not, enter no: ").strip().lower()
+
+    if digital_pin == "no":
+        digital_pin = None
+    else:
+        try:
+            digital_pin = int(digital_pin)
+        except ValueError:
+            print("Invalid number for led pin")
+            exit()
+    if servo_pin == "no":
+        servo_pin = None
+    else:
+        try:
+            servo_pin = int(servo_pin)
+        except ValueError:
+            print("Invalid number for motor pin")
+            exit()
+    if wc_plugged not in ("yes", "no"):
+        print("Invalid yes/no for webcam state")
+        exit()
+    return digital_pin, servo_pin, wc_plugged
+
+
 def digPinON(digital_pin, board):
     """
     digPin_ON turns on the designated digitalPin
@@ -66,6 +99,27 @@ def servoAngle(angle, servo_pin, board):
     print(f"{angle} Achieved")
 
 
+def stepperTest(dir_pin, step_pin, board):
+    # Move forward
+    delay = 0.002  # Seconds
+
+    step_angle = 18  # Degrees
+    steps_per_rev = 360 / step_angle  # 1 Full Motor Rotation -> 20 steps at 18 degrees
+
+    lead_screw_pitch = 0.5  # Millimeters
+    steps_per_mm = steps_per_rev/lead_screw_pitch  # Steps to Move Approx 1 Millimeter -> 40 Steps at 0.5 screw pitch
+
+    board.digital[dir_pin].mode = 1
+    board.digital[step_pin].mode = 1
+    board.digital[dir_pin].write(1)
+
+    for _ in range(int(steps_per_mm*4)):
+        board.digital[step_pin].write(1)
+        # time.sleep(delay)
+        board.digital[step_pin].write(0)
+        time.sleep(delay)
+
+
 def on_closing(board, win):
     """
     on_closing ensures both the Ardunio's COM Port and Tkinter's window closes
@@ -79,3 +133,4 @@ def on_closing(board, win):
 
 def placeHolder():
     print("This is placeholder text")
+
